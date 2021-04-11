0bcded3053706c663081b4b3966705438eb1696a
HerrZatacke <zatacke@gmail.com>
2021-04-11 17:30:26 +0200

    add test for inverted palette with locked frame

--- src/tests/tools/Decoder/test.js
+++ src/tests/tools/Decoder/test.js
@@ -75,4 +75,22 @@ describe('Decoder', () => {
     expect(putImageData.mock.calls[0][0].height).toBe(144); // y dimension
     expect(putImageData.mock.calls[0][0].data).toMatchSnapshot();
   });
+
+
+  it('renders an image with a inverted palette without affecting the frame', () => {
+    decoder.update({
+      canvas,
+      tiles: dummy,
+      palette: ['#ffd484', '#6ab300', '#006262', '#44005f'],
+      invertPalette: true,
+      lockFrame: true,
+    });
+
+    expect(putImageData.mock.calls.length).toBe(1); // calle only once
+    expect(putImageData.mock.calls[0][1]).toBe(0); // x coordinate
+    expect(putImageData.mock.calls[0][2]).toBe(0); // y coordinate
+    expect(putImageData.mock.calls[0][0].width).toBe(160); // x dimension
+    expect(putImageData.mock.calls[0][0].height).toBe(144); // y dimension
+    expect(putImageData.mock.calls[0][0].data).toMatchSnapshot();
+  });
 });
